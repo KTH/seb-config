aa7e948a2d98f87dff50acac474c64bfd1aab578
Emil Stenberg <esandin@gmail.com>
2022-09-27 14:23:54 +0200

    added comments

--- routes/utils.js
+++ routes/utils.js
@@ -1,5 +1,6 @@
 const PROXY_PATH_PREFIX = process.env.PROXY_PATH_PREFIX || '/';
 
+// TODO: It's difficult for me to understand what this anonymous function does. Can you somehow describe what the purpose of this function is? Either by naming the function, or adding a comment?
 module.exports.proxyPath = (enpointPath) => {
     let sep="/";
     if (PROXY_PATH_PREFIX.endsWith("/") || enpointPath.startsWith("/")) {
