d100ee5ba6bed761f5a75f77b3ddb1d9e47e46c9
Sebastian Ware <sebastian@urbantalk.se>
2022-09-27 17:48:55 +0200

    Added missing "/" for routes

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -4,13 +4,17 @@ const logger            = require('pino')()
 const express = require('express')
 const router = express.Router({ mergeParams: true })
 
-router.get('', function (req, res) {
+router.get('/', function (req, res) {
   res.redirect(proxyPath('/config'))
 })
 
 // universal404
-router.get('404', function (req, res) {
+router.get('/404', function (req, res) {
   res.render('fourOhFour')
+})
+
+router.get('/favicon.ico', function (req, res) {
+  res.sendStatus(200);
 })
 
 // catchall for 404
