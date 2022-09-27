34513847592c5266eedac09a3169b3f46b6bc58c
Krzysztof Starzecki <krzsta@kth.se>
2022-09-27 20:30:28 +0200

    more path shenanigans

--- routes/configRoutes.js
+++ routes/configRoutes.js
@@ -1,5 +1,6 @@
 const express   = require('express')
 const sebCg     = require('../configGen.js')
+const { proxyPath } = require('./utils');
 const convert   = require('xml-js')
 const logger            = require('pino')()
       
